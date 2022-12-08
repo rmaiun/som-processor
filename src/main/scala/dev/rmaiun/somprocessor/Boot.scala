b92911351889e35fac342f810bc51305e7679177
Roman Maiun <roman.maiun@unicorn.com>
2022-12-08 16:01:36 +0200

    consumers

--- src/main/scala/dev/rmaiun/somprocessor/Boot.scala
+++ src/main/scala/dev/rmaiun/somprocessor/Boot.scala
@@ -1,7 +1,10 @@
 package dev.rmaiun.somprocessor
 
-import cats.effect.{ ExitCode, IO, IOApp }
-import dev.rmaiun.somprocessor.services.EventProducersProvider
+import cats.effect.{ ExitCode, IO, IOApp, Ref }
+import dev.rmaiun.somprocessor.repositories.{ AlgorithmLockRepository, AlgorithmRepository, OptimizationRunRepository }
+import dev.rmaiun.somprocessor.services.RabbitInitializer.{ AlgorithmCode, AmqpStructures }
+import dev.rmaiun.somprocessor.services._
+import fs2.concurrent.SignallingRef
 import fs2.kafka.vulcan.{ AvroSettings, SchemaRegistryClientSettings }
 object Boot extends IOApp {
 
@@ -10,10 +13,30 @@ object Boot extends IOApp {
       SchemaRegistryClientSettings[IO]("http://localhost:8081")
     }
 
-  override def run(args: List[String]): IO[ExitCode] =
+  override def run(args: List[String]): IO[ExitCode] = {
+    val cfg = ConfigProvider.provideConfig
     for {
-      eventProducers <- EventProducersProvider.provide
+      eventProducers    <- EventProducersProvider.provide
+      rabbitConnections <- Ref.of[IO, Map[AlgorithmCode, AmqpStructures[IO]]](Map())
 
+      algorithmRepo     <- IO(AlgorithmRepository[IO]())
+      algorithmLockRepo <- IO(AlgorithmLockRepository[IO]())
+      optimizationRepo  <- IO(OptimizationRunRepository[IO]())
+      somAlg4Signal     <- SignallingRef.of[IO, Boolean](false)
+      map                = Map("ALG_SOM4_DEV" -> somAlg4Signal)
+
+      optModifier        <- OptimizationRunModifier.impl[IO](optimizationRepo)
+      optReqProcessor    <- OptimizationRunRequestProcessor.impl[IO](algorithmRepo, algorithmLockRepo, optimizationRepo, eventProducers)
+      somConnProvider    <- SomConnectionProvider.impl[IO](cfg, rabbitConnections, eventProducers, map, algorithmLockRepo)
+      somInFileGenerator <- SomInputFileGenerator.impl[IO](eventProducers)
+      somReqSender       <- SomRequestSender.impl[IO](rabbitConnections)
+
+      _ <- EventConsumersProvider.updateOptimizationRunEventConsumer(optModifier).start
+      _ <- EventConsumersProvider.generateInputDocumentProcessingEventConsumer(somInFileGenerator).start
+      _ <- EventConsumersProvider.createSomConnectionEventConsumer(somConnProvider).start
+      _ <- EventConsumersProvider.sendSomRequestEventConsumer(somReqSender).start
+      _ <- IO.never
     } yield ExitCode.Success
+  }
 
 }
--- src/main/scala/dev/rmaiun/somprocessor/services/EventConsumersProvider.scala
+++ src/main/scala/dev/rmaiun/somprocessor/services/EventConsumersProvider.scala
@@ -1,5 +1,58 @@
 package dev.rmaiun.somprocessor.services
 
+import cats.effect.IO
+import dev.rmaiun.somprocessor.domains.OptimizationRun
+import dev.rmaiun.somprocessor.events.OptimizationRunUpdateEvent
+import dev.rmaiun.somprocessor.events.ProcessingEvent.{ CreateSomConnectionCodec, GenerateInputDocumentProcessingEventCodec, SendSomRequestCodec }
+import fs2.kafka._
+
 object EventConsumersProvider {
+
+  def generateInputDocumentProcessingEventConsumer(processor: SomInputFileGenerator[IO]): IO[Unit] =
+    KafkaConsumer
+      .stream(consumerSettings(GenerateInputDocumentProcessingEventCodec.deserializer))
+      .subscribeTo(OptimizationRun.generateInputFileTopic)
+      .records
+      .evalTap(r => processor.generateInputDocument(r.record.value))
+      .compile
+      .drain
+
+  def createSomConnectionEventConsumer(processor: SomConnectionProvider[IO]): IO[Unit] =
+    KafkaConsumer
+      .stream(consumerSettings(CreateSomConnectionCodec.deserializer))
+      .subscribeTo(OptimizationRun.createSomConnectionTopic)
+      .records
+      .evalTap(r => processor.createSomConnection(r.record.value))
+      .compile
+      .drain
+
+  def sendSomRequestEventConsumer(processor: SomRequestSender[IO]): IO[Unit] =
+    KafkaConsumer
+      .stream(consumerSettings(SendSomRequestCodec.deserializer))
+      .subscribeTo(OptimizationRun.sendSomInputTopic)
+      .records
+      .evalTap(r => processor.sendSomRequest(r.record.value))
+      .compile
+      .drain
+
+  def updateOptimizationRunEventConsumer(processor: OptimizationRunModifier[IO]): IO[Unit] =
+    KafkaConsumer
+      .stream(consumerSettings(OptimizationRunUpdateEvent.codec.deserializer))
+      .subscribeTo(OptimizationRun.updateOptimizationRunTopic)
+      .partitionedRecords
+      .evalTap { chunk =>
+        chunk.compile.toList.flatMap(list => processor.applyBatchUpdate(list.map(_.record.value)))
+      }
+      .compile
+      .drain
+
+  private def consumerSettings[T](deserializer: RecordDeserializer[IO, T]): ConsumerSettings[IO, String, T] =
+    ConsumerSettings(
+      keyDeserializer = Deserializer[IO, String],
+      valueDeserializer = deserializer
+    )
+      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers("localhost:9092")
+      .withGroupId("group")
 
 }
